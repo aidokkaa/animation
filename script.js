ea8221e4018e3764839bb26e47f1c969a826a86a
Aida <aida.ms0097@gmail.com>
2024-06-08 15:48:58 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function upperCase() {
+    const x = document.querySelector("input");
+    x.value = x.value.toUpperCase();
+  }
+
+let red= document.querySelector('.red')
+  red.onclick = function (){
+  red.style.background= 'blue'
+  red.innerHTML='<h2>Text</h2>'
+}
+
+
+
+
+
+
+
